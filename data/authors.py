04f95d574e3d1eba6ff8cc9fe40897eb4c17b3f2
mariva <mariva.yoga@gmail.com>
2023-03-21 16:16:25 +0300

    autorisation

---
+++ data/authors.py
@@ -0,0 +1,15 @@
+import sqlalchemy
+from sqlalchemy import orm
+
+from .db_session import SqlAlchemyBase
+
+
+class Authors(SqlAlchemyBase):
+    __tablename__ = 'authors'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    surname = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    nickname = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    about = sqlalchemy.Column(sqlalchemy.Text, nullable=True)
+    library = orm.relationship("Library", backref='authors')
---
+++ data/library.py
@@ -0,0 +1,21 @@
+import sqlalchemy
+from sqlalchemy import orm
+
+from .db_session import SqlAlchemyBase
+
+
+class Library(SqlAlchemyBase):
+    __tablename__ = 'library'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    picture = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    genre = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    link_to_the_production = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    link_to_the_book = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    link_to_audio = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    link_to_the_screenshot = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    summary = sqlalchemy.Column(sqlalchemy.Text, nullable=True)
+    history_of_creation = sqlalchemy.Column(sqlalchemy.Text, nullable=True)
+    author_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("authors.id"))
+    user = orm.relationship('Authors')
