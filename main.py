a1734dcf819e96c85daebbb5ffe61a91ba1d9ee2
mariva <mariva.yoga@gmail.com>
2023-03-27 20:38:06 +0300

    home

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, redirect, request, abort
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from forms.user import RegisterForm, LoginForm
 from data.users import User
+from data.library import Library
+from data.authors import Authors
 from data import db_session
 
 app = Flask(__name__)
@@ -68,7 +70,10 @@ def login():
 @app.route("/")
 def index():
     if current_user.is_authenticated:
-        return render_template("home.html")
+        db_sess = db_session.create_session()
+        books = db_sess.query(Library).filter(Library.count_marks != 0).all()
+        books.sort(key=lambda i: (i.count_marks != 0, i.summa_marks / i.count_marks))
+        return render_template("home.html", sp_books=books)
     else:
         return render_template('index.html')
 
@@ -77,6 +82,19 @@ def index():
 def user_page():
     if current_user.is_authenticated:
         return render_template("user_page.html", nickname=current_user.nickname)
+
+@app.route("/add_book")
+def add_book():
+    return render_template("add_book.html")
+
+@app.route("/for_you")
+def for_you():
+    return render_template("for_you.html")
+
+
+@app.route("/book/<int:id>")
+def book(id):
+    ...
 
 if __name__ == '__main__':
     main()
