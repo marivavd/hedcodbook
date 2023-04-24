bd428c3776f0109c5586846ed37154336fc309fe
Nikita <Entvk20071@mail.ru>
2023-04-24 22:49:14 +0300

    Марина, посмотри

--- api/main_api.py
+++ api/main_api.py
@@ -143,8 +143,13 @@ def add_author():
         name=form.get('name'),
         surname=form.get('surname'),
         about=form.get('about'),
-        picture=form.get('picture'))
+        picture=f'{form.get("name")}_{form.get("surname")}')
 
     db_sess = db_session.create_session()
     db_sess.add(author)
     db_sess.commit()
+
+    f = request.files['picture']
+    photo_file = open(f'static/img/authors/{form.get("name")}_{form.get("surname")}.png', "wb")
+    photo_file.write(f.read())
+    photo_file.close()
